f73609d29e2cb2362ad4bf16b1502e7bb73e9ecc
Ben amor oussema <ousemabenamor100@outlook.fr>
2025-02-25 20:12:50 +0100

    chore: step-2 create ut to ensure existing functionalities

--- csharp/SupermarketReceipt.Test/SupermarketXUnitTest.cs
+++ csharp/SupermarketReceipt.Test/SupermarketXUnitTest.cs
@@ -36,5 +36,100 @@ namespace SupermarketReceipt.Test
             Assert.Equal(2.5 * 1.99, receiptItem.TotalPrice);
             Assert.Equal(2.5, receiptItem.Quantity);
         }
+
+        [Fact]
+        public void ThreeForTwoDiscrount()
+        {
+            // Arrange
+            SupermarketCatalog catalog = new FakeCatalog();
+
+            var toothbrush = new Product("toothbrush", ProductUnit.Each);
+            catalog.AddProduct(toothbrush, 1);
+
+            var cart = new ShoppingCart();
+            cart.AddItemQuantity(toothbrush, 3);
+
+            var teller = new Teller(catalog);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 3);
+
+            // ACT
+            var receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(2, receipt.GetTotalPrice());
+            var discount = new List<Discount>()
+            {
+                new Discount(toothbrush,"3 for 2",-1)
+            };
+            Assert.Equivalent(discount, receipt.GetDiscounts());
+            Assert.Single(receipt.GetItems());
+            var receiptItem = receipt.GetItems()[0];
+            Assert.Equal(toothbrush, receiptItem.Product);
+            Assert.Equal(1, receiptItem.Price);
+            Assert.Equal(3, receiptItem.TotalPrice);
+            Assert.Equal(3, receiptItem.Quantity);
+        }
+
+        [Fact]
+        public void TwoForAmountDiscrount()
+        {
+            // Arrange
+            SupermarketCatalog catalog = new FakeCatalog();
+
+            var toothbrush = new Product("toothbrush", ProductUnit.Each);
+            catalog.AddProduct(toothbrush, 1);
+
+            var cart = new ShoppingCart();
+            cart.AddItemQuantity(toothbrush, 2);
+
+            var teller = new Teller(catalog);
+            teller.AddSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 2);
+            // ACT
+            var receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(2, receipt.GetTotalPrice());
+            var discount = new List<Discount>()
+            {
+                new Discount(toothbrush,"2 for 2.00", 0)
+            };
+            Assert.Equivalent(discount, receipt.GetDiscounts());
+            Assert.Single(receipt.GetItems());
+            var receiptItem = receipt.GetItems()[0];
+            Assert.Equal(toothbrush, receiptItem.Product);
+            Assert.Equal(1, receiptItem.Price);
+            Assert.Equal(2, receiptItem.TotalPrice);
+            Assert.Equal(2, receiptItem.Quantity);
+        }
+
+        [Fact]
+        public void FiveForAmountDiscrount()
+        {
+            // Arrange
+            SupermarketCatalog catalog = new FakeCatalog();
+
+            var toothbrush = new Product("toothbrush", ProductUnit.Each);
+            catalog.AddProduct(toothbrush, 1);
+
+            var cart = new ShoppingCart();
+            cart.AddItemQuantity(toothbrush, 6);
+
+            var teller = new Teller(catalog);
+            teller.AddSpecialOffer(SpecialOfferType.FiveForAmount, toothbrush, 5);
+
+            // ACT
+            var receipt = teller.ChecksOutArticlesFrom(cart);
+
+            Assert.Equal(6, receipt.GetTotalPrice());
+            var discount = new List<Discount>()
+            {
+                new Discount(toothbrush,"5 for 5.00", 0)
+            };
+            Assert.Equivalent(discount, receipt.GetDiscounts());
+            Assert.Single(receipt.GetItems());
+            var receiptItem = receipt.GetItems()[0];
+            Assert.Equal(toothbrush, receiptItem.Product);
+            Assert.Equal(1, receiptItem.Price);
+            Assert.Equal(6, receiptItem.TotalPrice);
+            Assert.Equal(6, receiptItem.Quantity);
+        }
     }
 }
